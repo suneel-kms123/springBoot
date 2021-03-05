7807a4105b2ea6d5e285f351e2eff8b777d38bb3
suneel-kms123 <suneel.kms123@gmail.com>
2021-03-05 23:23:17 +0000

    kafka integration with docker compose and code cleanup - spring boots up but connection fails with kafka docker

--- src/main/java/com/programspring/SpringBoot/SwaggerConfiguration.java
+++ src/main/java/com/programspring/SpringBoot/SwaggerConfiguration.java
@@ -3,7 +3,13 @@ package com.programspring.SpringBoot;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
-@EnableSwagger
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
 @EnableAutoConfiguration
 public class SwaggerConfiguration {
 
--- src/main/webapp/index.jsp
+++
@@ -1,6 +0,0 @@
-<html>
-<body>
-<h2>Hello World!</h2>
-<a href="hello.html">click</a>  
-</body>
-</html>
